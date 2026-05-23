3210732a185a4116bbf70d220fc3c5ef2e413744
toribiodepaula-max <toribiodepaula@gmail.com>
2026-05-22 22:35:18 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mercadopago = require('mercadopago');
-
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mercadopago.configure({
